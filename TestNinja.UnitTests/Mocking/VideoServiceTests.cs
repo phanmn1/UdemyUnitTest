deb308c79befa71567ce214849c368c7d076c603
Minh Phan <mpflamingmarshmallow@gmail.com>
2021-04-24 12:54:37 -0700

    Move test code into files and Module 5 almost done

---
+++ TestNinja.UnitTests/Mocking/VideoServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using TestNinja.Mocking;
+using Moq;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    class VideoServiceTests
+    {
+        private VideoService _videoService;
+        private Mock<IFileReader> _fileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_fileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_EmptyFile_ReturnError()
+        {
+            _fileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+            
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
